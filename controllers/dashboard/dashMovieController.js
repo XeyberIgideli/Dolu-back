dc0912b95f976324f0c1afe980e05d62b4c9bd44
XeyberIgideli <d3facerx@gmail.com>
2023-06-25 23:45:44 +0400

    fixed some bugs in adding movie

--- controllers/dashboard/dashMovieController.js
+++ controllers/dashboard/dashMovieController.js
@@ -17,24 +17,33 @@ class movieController {
         }
         const files = req.files
         let arr = {}
+        let uniqueImageName
+        let uploadedImage
+        let imageExt
+        let uploadPath
+        let moveFiles = {}
         var hasError = true
         let genresArr = req.body['genres[]'] ? req.body['genres[]'].split(',') : null
         let directorArr = req.body.director ? req.body.director.split(',') : null
-
+        
+        Object.keys(files).forEach(file => {
+            uploadedImage = files[file]
+            imageExt = uploadedImage.name.substring(uploadedImage.name.lastIndexOf('.'))
+            uniqueImageName = uniqueID(uploadedImage.name.substring(0,uploadedImage.name.lastIndexOf('.')),8)
+            uploadPath = globalDirName + '/public/uploads/movie/' + uniqueImageName + imageExt
+            arr[file] = uploadPath
+        })
+        
         const movie = await Movie.create({...req.body,...arr,genres:genresArr,director: directorArr})
         hasError = false
-        
-        for(let file in files) {
-                let uploadedImage = req.files[file]
-                let imageExt = uploadedImage.name.substring(uploadedImage.name.lastIndexOf('.'))
-                let uniqueImageName = uniqueID(uploadedImage.name.substring(0,uploadedImage.name.lastIndexOf('.')),8)
-                let uploadPath = globalDirName + '/public/uploads/movie/' + uniqueImageName + imageExt
-                
-                arr[file] = uploadPath
-                if(!hasError) {
-                    await uploadedImage.mv(uploadPath)
-                }
-        } 
+
+        if(!hasError) {
+           for(let file in files) {  
+             await files[file].mv(arr[file])
+           } 
+        }
+
+        // res.redirect('../movies')
 
     } catch(err) {
         next(err)
