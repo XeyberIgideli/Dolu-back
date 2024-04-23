6d9809a1a72da37afb7de1573724886300b272bd
Igidali <d3facerx@gmail.com>
2024-04-23 18:07:17 +0400

    removed torrent-search-api

--- utils/torrent.js
+++ utils/torrent.js
@@ -1,23 +1,23 @@
-import TorrentSearchApi from 'torrent-search-api'
-const activeProviders = TorrentSearchApi.getActiveProviders();
-TorrentSearchApi.enableProvider('1337x')
+// import TorrentSearchApi from 'torrent-search-api'
+// const activeProviders = TorrentSearchApi.getActiveProviders();
+// TorrentSearchApi.enableProvider('1337x')
 
 async function torrentSearch(title,index) {  
-      const torrents = await TorrentSearchApi.search(title, 'All', 5)
-      if(torrents.length === 0) {
-            console.log('No torrent found!')
-            return;
-      } 
-      const torrentFiltered = torrents.filter(torrent => {
-           let sizeInMB = parseFloat(torrent.size)
-           if (torrent.size.includes("GB")) {
-            sizeInMB *= 1024; // 1 GB = 1024 MB
-          }
-           return torrent.seeds > -1 && sizeInMB <= 1500 && !torrent.title.match(/\bXviD\b/) 
-      })  
-      const magnet = await TorrentSearchApi.getMagnet(torrentFiltered[index]) 
+      // const torrents = await TorrentSearchApi.search(title, 'All', 5)
+      // if(torrents.length === 0) {
+      //       console.log('No torrent found!')
+      //       return;
+      // } 
+      // const torrentFiltered = torrents.filter(torrent => {
+      //      let sizeInMB = parseFloat(torrent.size)
+      //      if (torrent.size.includes("GB")) {
+      //       sizeInMB *= 1024; // 1 GB = 1024 MB
+      //     }
+      //      return torrent.seeds > -1 && sizeInMB <= 1500 && !torrent.title.match(/\bXviD\b/) 
+      // })  
+      // const magnet = await TorrentSearchApi.getMagnet(torrentFiltered[index]) 
 
-      return magnet
+      // return magnet
 } 
 
 export {torrentSearch}
