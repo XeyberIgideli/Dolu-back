413b1ac12d245beaa670ee7bd32d5e5d3ad36a96
Igidali <d3facerx@gmail.com>
2024-05-03 21:49:56 +0400

    changed torrent search package with my own

--- utils/torrent.js
+++ utils/torrent.js
@@ -1,23 +1,18 @@
-// import TorrentSearchApi from 'torrent-search-api'
-// const activeProviders = TorrentSearchApi.getActiveProviders();
-// TorrentSearchApi.enableProvider('1337x')
+import torrentApi from "@pardaillan/torrent-api"
+
+
+const torrentSource =  await torrentApi.setSource("1377x")
 
 async function torrentSearch(title,index) {  
-      // const torrents = await TorrentSearchApi.search(title, 'All', 5)
-      // if(torrents.length === 0) {
-      //       console.log('No torrent found!')
-      //       return;
-      // } 
-      // const torrentFiltered = torrents.filter(torrent => {
-      //      let sizeInMB = parseFloat(torrent.size)
-      //      if (torrent.size.includes("GB")) {
-      //       sizeInMB *= 1024; // 1 GB = 1024 MB
-      //     }
-      //      return torrent.seeds > -1 && sizeInMB <= 1500 && !torrent.title.match(/\bXviD\b/) 
-      // })  
-      // const magnet = await TorrentSearchApi.getMagnet(torrentFiltered[index]) 
-
-      // return magnet
+      const torrents = await torrentSource.search({query: title, size: 1300})
+      if(torrents.length === 0) {
+            console.log('No torrent found!')
+            return;
+      } 
+     
+      const magnet = await torrentSource.getMagnet(torrents[index])  
+      
+      return magnet
 } 
-
+ 
 export {torrentSearch}
